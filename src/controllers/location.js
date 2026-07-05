a05d3e0823f869212c41ca20b0207c91f0772282
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2026-07-06 00:09:25 +0300

    feat: add getStates tests and update location controller (#86)

--- src/controllers/location.js
+++ src/controllers/location.js
@@ -1,32 +1,20 @@
 const locationService = require('~/services/location')
 
-const getCountries = async (req, res, next) => {
-  try {
-    const countries = await locationService.getCountries()
-    res.status(200).json(countries)
-  } catch (error) {
-    next(error)
-  }
+const getCountries = async (req, res) => {
+  const countries = await locationService.getCountries()
+  res.status(200).json(countries)
 }
 
-const getStates = async (req, res, next) => {
-  try {
-    const { countryCode } = req.query
-    const states = await locationService.getStates(countryCode)
-    res.status(200).json(states)
-  } catch (error) {
-    next(error)
-  }
+const getStates = async (req, res) => {
+  const { countryCode } = req.query
+  const states = await locationService.getStates(countryCode)
+  res.status(200).json(states)
 }
 
-const getCities = async (req, res, next) => {
-  try {
-    const { countryCode, stateCode } = req.query
-    const cities = await locationService.getCities(countryCode, stateCode)
-    res.status(200).json(cities)
-  } catch (error) {
-    next(error)
-  }
+const getCities = async (req, res) => {
+  const { countryCode, stateCode } = req.query
+  const cities = await locationService.getCities(countryCode, stateCode)
+  res.status(200).json(cities)
 }
 
 module.exports = {
